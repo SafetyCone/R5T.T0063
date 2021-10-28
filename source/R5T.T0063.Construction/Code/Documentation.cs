8584c8c64db131a76c61affd01a034c8a498d104
davidcoats <david@getsafetycone.com>
2021-10-28 11:21:54 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0063.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0063.Construction
+{
+    /// <summary>
+    /// Construction project for ISeriviceAction<T> types..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0063/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0063
+{
+    /// <summary>
+    /// ISeriviceAction<T> types.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
